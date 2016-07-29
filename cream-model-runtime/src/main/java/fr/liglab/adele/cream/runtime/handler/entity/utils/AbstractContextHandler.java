636f272adc1c02d3d537394202eda5e1d3b26672
aygalinc <colin.aygalinc@gmail.com>
2016-07-29 08:48:37 +0200

    Refactor common code

--- cream-model-runtime/src/main/java/fr/liglab/adele/cream/runtime/handler/entity/utils/AbstractContextHandler.java
+++ cream-model-runtime/src/main/java/fr/liglab/adele/cream/runtime/handler/entity/utils/AbstractContextHandler.java
@@ -1,5 +1,8 @@
 package fr.liglab.adele.cream.runtime.handler.entity.utils;
 
+import fr.liglab.adele.cream.annotations.ContextService;
+import fr.liglab.adele.cream.annotations.State;
+import fr.liglab.adele.cream.annotations.internal.HandlerReference;
 import fr.liglab.adele.cream.model.ContextEntity;
 import org.apache.felix.ipojo.ConfigurationException;
 import org.apache.felix.ipojo.InstanceManager;
@@ -10,7 +13,10 @@ import org.apache.felix.ipojo.util.Property;
 import org.wisdom.api.concurrent.ManagedScheduledExecutorService;
 import org.wisdom.api.concurrent.ManagedScheduledFutureTask;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
@@ -18,6 +24,23 @@ import java.util.function.Consumer;
  * Created by aygalinc on 19/07/16.
  */
 public abstract class AbstractContextHandler extends PrimitiveHandler implements ContextEntity {
+
+
+    /**
+     * The list of exposed context services
+     */
+    protected final Set<String> services 					= new HashSet<>();
+
+    /**
+     * The list of states defined in the implemented context services
+     */
+    protected final Set<String> stateIds 					= new HashSet<>();
+
+    /**
+     * The list of interceptors in charge of handling each state field
+     */
+    protected final Set<StateInterceptor>	interceptors	= new HashSet<>();
+
 
     /**
      * Get The scheduler
@@ -34,6 +57,193 @@ public abstract class AbstractContextHandler extends PrimitiveHandler implements
      */
     abstract public void update(String stateId, Object value);
 
+
+
+    /**
+     * Handler Configuration
+     *
+     **/
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void configure(Element element, Dictionary rawConfiguration) throws ConfigurationException {
+
+
+        InstanceManager instanceManager 		= getInstanceManager();
+        String componentName					= instanceManager.getInstanceName();
+
+        Dictionary<String,Object> configuration	= (Dictionary<String,Object>) rawConfiguration;
+
+        /*
+         * Introspect interfaces implemented by the component POJO and construct the
+         * state specification of the entity ( basically a set of state variable)
+         */
+        for(Class<?> service : getInstanceManager().getClazz().getInterfaces()) {
+            extractDefinedStatesForService(service);
+
+            /**
+             * keeps only exposed context services
+             */
+            if (service.isAnnotationPresent(ContextService.class)) {
+                services.add(service.getName());
+            }
+        }
+
+		/*
+		 * Initialize interceptors for the different field management policies
+		 */
+        SynchronisationInterceptor synchronisationInterceptor 	= new SynchronisationInterceptor(this);
+        DirectAccessInterceptor directAccessInterceptor 		= new DirectAccessInterceptor(this);
+
+        interceptors.add(synchronisationInterceptor);
+        interceptors.add(directAccessInterceptor);
+
+        /*
+         * Parse the manifest and compare if all the state variable declared in the specification are referenced in the implementation.
+         * Add the appropriate interceptors to fields and methods
+         */
+
+        List<String> implementedStates = new ArrayList<String>();
+        for (Element entity : optional(element.getElements(HandlerReference.BEHAVIOR_ENTITY_HANDLER,HandlerReference.NAMESPACE))) {
+            for (Element state : optional(entity.getElements("state"))) {
+
+                String stateId 			= state.getAttribute("id");
+
+                if (stateId == null) {
+                    throw new ConfigurationException("Malformed Manifest : a state variable is declared with no 'id' attribute");
+                }
+
+                if (! stateIds.contains(stateId)) {
+                    throw new ConfigurationException("Malformed Manifest : the state "+stateId+" is not defined in the implemented context services");
+                }
+
+                if (implementedStates.contains(stateId)) {
+                    throw new ConfigurationException("Malformed Manifest : several state variable are declared for the same state "+stateId);
+                }
+
+                implementedStates.add(stateId);
+
+				/*
+				 * Add field and method interceptors according to the specified policy
+				 */
+                boolean directAccess	= Boolean.valueOf(state.getAttribute("directAccess"));
+                if (!directAccess) {
+                    synchronisationInterceptor.handleState(instanceManager,getPojoMetadata(),state);
+                }
+
+                else {
+                    directAccessInterceptor.handleState(instanceManager,getPojoMetadata(),state);
+                }
+
+    			/*
+    			 * If there is no specified value, but a default was specified for the field, use it
+    			 */
+                Object configuredValue 	= getStateConfiguredValue(stateId,configuration);
+                if (configuredValue == null) {
+
+                    String defaultValue 	= state.getAttribute("value");
+                    boolean hasDefaultValue	=  !fr.liglab.adele.cream.annotations.entity.ContextEntity.State.Field.NO_VALUE.equals(defaultValue);
+                    if (hasDefaultValue) {
+                        configuredValue = defaultValue;
+                        setStateConfiguredValue(stateId,defaultValue,configuration);
+                    }
+                }
+
+                /*
+                 * validate configured values are correctly typed for the field
+                 */
+                String stateField			= state.getAttribute("field");
+                FieldMetadata fieldMetadata = getPojoMetadata().getField(stateField);
+                boolean isValid 			= configuredValue == null || hasValidType(instanceManager,fieldMetadata,configuredValue);
+
+				/*
+				 * If the configured value doesn't have the right type, but it is an String, try to cast it
+				 */
+                if ( (!isValid) && configuredValue != null && (configuredValue instanceof String)) {
+                    Object cast = cast(instanceManager,fieldMetadata,(String)configuredValue);
+                    if (cast != null) {
+                        configuredValue = cast;
+                        isValid			= true;
+                        setStateConfiguredValue(stateId, configuredValue, configuration);
+                    }
+                }
+
+                if (! isValid) {
+                    throw new ConfigurationException("The configured value for state "+stateId+" doesn't match the type of the field :"+ configuredValue);
+                }
+            }
+        }
+
+        /*
+         * Check that all states defined in the specification are implemented
+         */
+        Set<String> unimplemented = new HashSet<>(stateIds);
+        unimplemented.removeAll(implementedStates);
+
+        if (! unimplemented.isEmpty()) {
+            throw new ConfigurationException("States " + unimplemented + " are defined in the context service, but never implemented in " + componentName);
+        }
+
+    	/*
+         * Check the context entity id was specified
+         */
+        if (configuration.get(CONTEXT_ENTITY_ID) == null) {
+            throw new ConfigurationException("Try to instantiate a context entity without and context.entity.id element");
+        }
+        stateIds.add(CONTEXT_ENTITY_ID);
+        update(CONTEXT_ENTITY_ID,configuration.get(CONTEXT_ENTITY_ID));
+
+        /*
+         * Initialize the state map with the configured values
+         */
+        for (String configuredState : getConfiguredStates(configuration)) {
+            if (stateIds.contains(configuredState)) {
+                update(configuredState, getStateConfiguredValue(configuredState, configuration));
+            }
+            else {
+                warn("Configured state " + configuredState + " will be ignored, it is not defined in the context services of " + componentName);
+            }
+        }
+    }
+
+
+    /**
+     * Get the definition of the states associated to a given context service
+     */
+    private void extractDefinedStatesForService(Class<?> service) {
+
+    	/*
+    	 * consider only context services
+    	 */
+        if (!service.isAnnotationPresent(ContextService.class)) {
+            return;
+        }
+
+        String contextServiceName = service.getAnnotation(ContextService.class).value();
+        if (contextServiceName.equals(ContextService.DEFAULT_VALUE)) {
+            contextServiceName = service.getSimpleName().toLowerCase();
+        }
+
+    	/*
+    	 * look for all states defined in the context service interface.
+    	 *
+    	 * The states of a service are defined by static, String fields marked with the annotation State.
+    	 * The value of the field is the name of the state.
+    	 *
+    	 */
+        for (Field field : service.getDeclaredFields()) {
+            if (Modifier.isStatic(field.getModifiers()) && field.getType().equals(String.class) && field.isAnnotationPresent(State.class)) {
+                try {
+                    String stateName = String.class.cast(field.get(null));
+                    stateIds.add(contextServiceName+"."+stateName);
+                } catch (IllegalArgumentException | IllegalAccessException ignored) {
+                }
+
+            }
+        }
+        for (Class<?> inheritedService : service.getInterfaces()){
+            extractDefinedStatesForService(inheritedService);
+        }
+    }
 
     /**
      * This class keeps track of all the information required to schedule periodic tasks
